1354bc460c18d28f1350aad8796eb54c27565a38
Chenhan Gao <chenhan.gao@duke.edu>
2026-04-17 23:09:24 -0400

    Revert "add ecommerce SQL dashboard portfolio project"
    This reverts commit db4f2f54b8089ee2c03f7430fcbbfe8a57bc1b28.

--- src/ecommerce_sql_dashboard/__init__.py
+++
@@ -1,6 +0,0 @@
-"""SQL-first e-commerce analytics portfolio project."""
-
-from .data import DATASET_URLS, ensure_raw_data
-from .pipeline import build_dashboard_artifacts
-
-__all__ = ["DATASET_URLS", "ensure_raw_data", "build_dashboard_artifacts"]
